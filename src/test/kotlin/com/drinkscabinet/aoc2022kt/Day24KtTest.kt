d76b100aee4447c8f7195f41e72f158544811acc
jwhiting <james@drinkscabinet.com>
2022-12-24 20:20:10 +0000

    day24part1

--- src/test/kotlin/com/drinkscabinet/aoc2022kt/Day24KtTest.kt
+++ src/test/kotlin/com/drinkscabinet/aoc2022kt/Day24KtTest.kt
@@ -40,7 +40,7 @@ class Day24KtTest {
     fun testPart1Real() {
         // 4495 too high
         //1536 too low
-        assertEquals(3684, solve(realData, false))
+        assertEquals(228, solve(realData, false))
     }
 
     private fun solve(data: String, part2: Boolean): Long {
@@ -65,6 +65,7 @@ class Day24KtTest {
 
         init {
             moveGrid.applyAll { _, c -> if (c == '#' || c == '.') c else '.' }
+            moveGrid[Coord(0, -2)] = '#'
         }
 
         val lcm = Utils.lcm(grid.getXMax(), grid.getYMax())
@@ -90,26 +91,40 @@ class Day24KtTest {
 
         fun simulate(turns: Long) {
             for (i in 0..turns) {
-                val grid2 = grid.copyOf()
+                val grid2 = moveGrid.copyOf()
                 grid2.applyAll { _, c -> if (c == '#' || c == '.') c else '.' }
                 for (b in blizzards) {
+//                    println("Blizzard: $b")
+                    val icon = icons[b.position.delta]
+//                    println("Position at time $i is ${b.positionAt(i)} icon is $icon")
                     grid2[b.positionAt(i)] = icons[b.position.delta]!!
+//                    println("Grid after is")
+//                    println(grid2.toString(nums = true))
                 }
-                println("Turn $i")
-                println(grid2)
+                println("After Turn $i")
+                println(grid2.toString(nums = true))
             }
         }
 
         fun solve(): Long {
-            simulate(10)
+            println("lcm is $lcm")
+//            simulate(lcm*2+1)
+//            println(State(9, Coord(5, 3)).nextStates())
             val startState = State(0, Coord(0, -1))
             val endState = State(-1, Coord(grid.getXMax() - 1, grid.getYMax()))
             val result = PathFinder.dijkstra2(startState, endState) { it.nextStates() }
-            println(result)
-            return result.first[endState]!!.toLong()
+            println(result.first.size)
+//            for( r in result.first ) {
+//                println(r)
+//            }
+            // find smallest result
+
+            return result.first.filter { it.key.pos == endState.pos }.minBy { it.value }.value.toLong()
+//            return result.first[endState]!!.toLong()
         }
 
         inner class State(val time: Long, val pos: Coord) {
+
 
             fun nextStates(): List<Pair<State, Int>> {
                 val next = mutableListOf<Pair<State, Int>>()
@@ -123,6 +138,7 @@ class Day24KtTest {
                     }
                 }
                 // Filter out ones where there will be a blizzard
+//                return next
                 return next.filter { !it.first.inBlizzard() }
             }
 
@@ -161,16 +177,29 @@ class Day24KtTest {
                 result = 31 * result + pos.hashCode()
                 return result
             }
+
+            override fun toString(): String {
+                return "State(time=$time, pos=$pos)"
+            }
         }
     }
 
     data class Blizzard(val position: Vector, val cycleLength: Long) {
         fun positionAt(time: Long): Coord {
-            var nextX = (position.pos.x + (position.delta.x * time)) % cycleLength
-            if (nextX < 0) nextX += cycleLength
-            var nextY = (position.pos.y + (position.delta.y * time)) % cycleLength
-            if (nextY < 0) nextY += cycleLength
-            return Coord(nextX, nextY)
+            if(horizontal) {
+                var nextX = (position.pos.x + (position.delta.x * time)) % cycleLength
+                if (nextX < 0) nextX += cycleLength
+                return Coord(nextX, position.pos.y)
+            }
+            else {
+                var nextY = (position.pos.y + (position.delta.y * time)) % cycleLength
+                if (nextY < 0) nextY += cycleLength
+                return Coord(position.pos.x, nextY)
+            }
+        }
+
+        override fun toString(): String {
+            return "Blizzard(position=$position, cycleLength=$cycleLength, horizontal=$horizontal, vertical=$vertical)"
         }
 
         val horizontal: Boolean
@@ -178,6 +207,7 @@ class Day24KtTest {
 
         val vertical: Boolean
             get() = position.delta.x == 0
+
 
         companion object {
             fun parse(grid: GridString) = sequence {
