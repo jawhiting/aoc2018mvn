7584208bb691355400f5e5d23918599d1cf0f562
jwhiting <james@drinkscabinet.com>
2024-12-06 08:19:43 +0000

    day6

--- src/test/kotlin/com/drinkscabinet/aoc2024/Day6KtTest.kt
+++ src/test/kotlin/com/drinkscabinet/aoc2024/Day6KtTest.kt
@@ -1,5 +1,8 @@
 package com.drinkscabinet.aoc2024
 
+import Direction
+import GridString
+import com.drinkscabinet.Coord
 import com.drinkscabinet.Utils
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -7,7 +10,16 @@ import org.junit.jupiter.api.Test
 
 class Day6KtTest {
 
-    private val testData = """"""
+    private val testData = """....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#..."""
 
 
     // Get the year and day from the class
@@ -16,21 +28,87 @@ class Day6KtTest {
 
     @Test
     fun testPart1() {
-        assertEquals(18, this.part1(testData))
-        assertEquals(2642, this.part1(realData))
+        assertEquals(41, this.part1(testData, false))
+        assertEquals(4988, this.part1(realData, false))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(9, this.part2(testData))
-        assertEquals(1974, this.part2(realData))
+        assertEquals(6, this.part1(testData, true))
+        assertEquals(1697, this.part1(realData, true))
     }
 
-    private fun part1(data: String): Int {
-        return 0
+    private fun part1(data: String, part2: Boolean): Int {
+        val grid = GridString.parse(data)
+        val grid2 = grid.copyOf()
+        val startPos = grid.getAll('^').first()
+        val startDir = Direction.N
+
+        var pos = startPos
+        var dir = startDir
+
+        while(grid.contains(pos)) {
+            // mark where we've been
+            grid2[pos] = 'X'
+            // Check next step
+            var nextInDir = pos + dir
+            if(grid[nextInDir] == '#') {
+                // crate encountered, we need to turn
+                dir = dir.rotate(1)
+                nextInDir = pos + dir
+            }
+            pos = nextInDir
+        }
+        // Now count all the X
+        if(!part2) return grid2.getAll('X').count()
+
+        // Now do part 2 - use the X positions as options
+        var options = grid2.getAll('X').toMutableSet()
+        options.remove(startPos)
+        var count = 0
+        var curr = 0
+        var total = options.size
+        for(c in options) {
+            val gridObstacle = grid.copyOf()
+            gridObstacle[c] = '#'
+            if(!exits(startPos, startDir, gridObstacle)) {
+                count++
+                println("Loops with $c")
+            }
+//            println("Tested ${curr++} out of $total")
+        }
+        return count
     }
 
-    private fun part2(data: String): Int {
-        return 0
+    private fun exits(start: Coord, startDir: Direction, grid: GridString) : Boolean {
+        // navigate the path, track vector at each coord
+        val visited = mutableMapOf(start to mutableSetOf(startDir))
+        val grid2 = grid.copyOf()
+        var pos = start
+        var dir = startDir
+
+        while(grid.contains(pos)) {
+            // mark where we've been
+            grid2[pos] = 'X'
+            // add to path
+            visited.getOrPut(pos){mutableSetOf<Direction>()}.add(dir)
+            // Check next step
+            var nextInDir = pos + dir
+            while(grid[nextInDir] == '#') {
+                // crate encountered, we need to turn
+                dir = dir.rotate(1)
+                nextInDir = pos + dir
+            }
+            pos = nextInDir
+            // if the new position is one we already visited, exit
+            if(visited.getOrDefault(pos, emptySet<Direction>()).contains(dir)) return false
+        }
+        // exited the grid
+        return true
     }
+
+//    @Test
+//    fun testExits() {
+//        var
+//    }
 }
