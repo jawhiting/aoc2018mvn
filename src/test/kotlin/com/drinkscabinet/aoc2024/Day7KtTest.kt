b1e700a8b4bb6c963d0aaee6a169f62531c41bce
jwhiting <james@drinkscabinet.com>
2024-12-07 17:25:48 +0000

    day7

--- src/test/kotlin/com/drinkscabinet/aoc2024/Day7KtTest.kt
+++ src/test/kotlin/com/drinkscabinet/aoc2024/Day7KtTest.kt
@@ -7,7 +7,15 @@ import org.junit.jupiter.api.Test
 
 class Day7KtTest {
 
-    private val testData = """"""
+    private val testData = """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
 
 
     // Get the year and day from the class
@@ -16,21 +24,47 @@ class Day7KtTest {
 
     @Test
     fun testPart1() {
-        assertEquals(18, this.part1(testData))
-        assertEquals(2642, this.part1(realData))
+        assertEquals(3749, this.part1(testData))
+        assertEquals(7579994664753, this.part1(realData))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(9, this.part2(testData))
-        assertEquals(1974, this.part2(realData))
+        assertEquals(11387, this.part2(testData))
+        assertEquals(438027111276610, this.part2(realData))
     }
 
-    private fun part1(data: String): Int {
-        return 0
+    private fun part1(data: String): Long {
+        return data.lines().map{Utils.extractLongs(it).toList()}.filter { matches(it[0], 0L, it.subList(1, it.size)) }.sumOf { it[0] }
     }
 
-    private fun part2(data: String): Int {
-        return 0
+    private fun part2(data: String): Long {
+        return data.lines().map{Utils.extractLongs(it).toList()}.filter { matches2(it[0], 0L, it.subList(1, it.size)) }.sumOf { it[0] }
+    }
+
+    fun matches(target: Long, runningTotal: Long, numbers: List<Long>) : Boolean {
+        if(numbers.isEmpty()) {
+            return target == runningTotal
+        }
+        val child = numbers.subList(1, numbers.size)
+        return matches(target, runningTotal * numbers[0], child) ||
+                matches(target, runningTotal + numbers[0], child)
+    }
+
+    fun matches2(target: Long, runningTotal: Long, numbers: List<Long>) : Boolean {
+        if(numbers.isEmpty()) {
+            return target == runningTotal
+        }
+        val child = numbers.subList(1, numbers.size)
+        return matches2(target, runningTotal * numbers[0], child) ||
+            matches2(target, runningTotal + numbers[0], child) ||
+            matches2(target, concat(runningTotal,numbers[0]), child)
+    }
+
+
+
+    fun concat(a: Long, b: Long): Long {
+        // concatenate two numbers together
+        return (a.toString() + b.toString()).toLong()
     }
 }
